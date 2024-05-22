4093a1b8d30eac227a24dabe787cb54a208ccbed
Igors <166141617+CBuPenblu@users.noreply.github.com>
2024-05-23 00:36:50 +0300

    add video4

---
+++ main4.py
@@ -0,0 +1,21 @@
+#class SmartHouse():
+#    def turn_on_light(self):
+#        pass
+#
+#    def heat_food(self):
+#        pass
+#
+#    def turn_on_music(self):
+#        pass
+
+class Light():
+    def turn_on_light(self):
+        print("Light is on")
+
+class Food():
+    def heat_up_food(self):
+        print("Food is started to heat")
+
+class Music():
+    def turn_on_music(self):
+        print("Turning on your favourite playlist")
